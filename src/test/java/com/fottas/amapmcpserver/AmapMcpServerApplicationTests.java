59e21e7421607f684f80c3552938b792882c83d9
yinh <yinh@133.cn>
2025-07-28 10:19:58 +0800

    init product

---
+++ src/test/java/com/fottas/amapmcpserver/AmapMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fottas.amapmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmapMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
